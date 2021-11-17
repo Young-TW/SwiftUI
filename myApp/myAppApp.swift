cc53ca3ef233e442630f488c2aa4678ec3cd780e
Young <young20050727@gmail.com>
2021-11-17 10:54:09 +0800

    Initial Commit

---
+++ myApp/myAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  myAppApp.swift
+//  myApp
+//
+//  Created by 王晨洋 on 2021/11/17.
+//
+
+import SwiftUI
+
+@main
+struct myAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
