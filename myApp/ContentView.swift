8f98f5fcbf7a9ffb2ea421d8ed829060c96aefc9
Young <young20050727@gmail.com>
2021-11-17 11:53:02 +0800

    swift app testing

--- myApp/ContentView.swift
+++ myApp/ContentView.swift
@@ -2,20 +2,33 @@
 //  ContentView.swift
 //  myApp
 //
-//  Created by 王晨洋 on 2021/11/17.
+//  Created by Young on 2021/11/17.
 //
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        let hello = "Hello, I am Young!"
+        VStack(alignment: .leading) {
+            Image("background-1")
+                .clipShape(Circle())
+                .frame(width: 5, height: 5)
+            Text(hello)
+                .font(.title)
+                .fontWeight(.heavy)
+            .foregroundColor(.blue)
+            HStack {
+                Text("I am a Student in NTIHS.")
+                    .font(.subheadline)
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+.previewInterfaceOrientation(.portrait)
     }
 }
