6df60901a7f1369c292bbd56ae005515d71729ed
Young <young20050727@gmail.com>
2021-11-28 02:18:27 +0800

    Update ContentView.swift

--- myApp/ContentView.swift
+++ myApp/ContentView.swift
@@ -8,27 +8,60 @@
 import SwiftUI
 
 struct ContentView: View {
+//    var redValue: Double
+//    var greeenValue: Double
+//    var blueValue: Double
     var body: some View {
-        let hello = "Hello, I am Young!"
-        VStack(alignment: .leading) {
-            Image("background-1")
-                .clipShape(Circle())
-                .frame(width: 5, height: 5)
+        let hello = "Hello!"
+        VStack() {
+//            Image("background-1")
+//                .clipShape(Circle())
+//                .frame(width: 5, height: 5)
             Text(hello)
                 .font(.title)
                 .fontWeight(.heavy)
-            .foregroundColor(.blue)
-            HStack {
-                Text("I am a Student in NTIHS.")
-                    .font(.subheadline)
+                .foregroundColor(.blue)
+                .padding()
+                .background(.black)
+            HStack{
+                Text("R:")
+                    .font(.title)
+                Spacer()
+                Text("G:")
+                    .font(.title)
+                Spacer()
+                Text("B:")
+                    .font(.title)
+            }.padding(70)
+                .offset(x:0, y:10)
+            Spacer()
+            Slider(value: Binding.constant(1))
+                .padding()
+            Slider(value: Binding.constant(1))
+                .padding()
+            Slider(value: Binding.constant(1))
+                .padding()
+        }.padding()
+    }
+}
+
+struct MapView: View {
+    var body: some View {
+        VStack{
+            Text("屌")
+            Toggle(isOn: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Is On@*/.constant(true)/*@END_MENU_TOKEN@*/) {
+            /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Label@*/Text("Label")/*@END_MENU_TOKEN@*/
             }
+                .padding()
         }
     }
 }
 
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+        // MapView()
 .previewInterfaceOrientation(.portrait)
     }
 }
